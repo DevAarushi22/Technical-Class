430c36256ea54542f0a1cf5f55667ae7efcf3c30
Aarushi <aarushisinha2005@gmail.com>
2024-09-10 19:30:23 +0530

    Day 2 C

---
+++ class1.cpp
@@ -0,0 +1,104 @@
+// #include<iostream>
+// #include<array>
+// using namespace std;
+
+// //array
+// int main(){
+//     int basic[3]={1,2,3};
+//     array<int,4> a={1,2,3,4};
+//     int size = a.size();
+
+//     for(int i=0; i<size; i++){
+//         cout<<a[i]<<endl;
+//     }
+
+//     cout<<"Element at 2nd Index:"<< a.at(2)<<endl;
+
+//     cout<<"Empty or not:"<<a.empty()<<endl;
+
+//     cout<<"First element->" <<a.front()<<endl;
+//     cout<<"last element:"<<a.back()<<endl;
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     vector<int> v;
+
+//     vector<int> a(5,1);
+
+//     vector<int> last(a);
+
+//     cout<<"print last"<<endl;
+
+//     for(int i:last){
+//         cout<<i<<endl;
+//     }
+//     cout<<endl;
+
+//     cout<<"Capacity-"<<v.capacity()<<endl;
+
+//     v.push_back(1);
+//     cout<<"Capacity-"<<v.capacity()<<endl;
+
+//     v.push_back(2);
+//     cout<<"Capacity-"<<v.capacity()<<endl;
+
+//     v.push_back(3);
+//     cout<<"Capacity-"<<v.capacity()<<endl;
+
+
+// }
