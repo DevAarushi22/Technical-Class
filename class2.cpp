10fbe88ba6fca3d5a4593424a85c5953af899e56
Aarushi <aarushisinha2005@gmail.com>
2024-09-14 00:57:18 +0530

    Vector and it fuction in C++. day 3

---
+++ class2.cpp
@@ -0,0 +1,6 @@
+// Given an array nums containing n distinct numbers in the range [0, n] , return
+// the only number in the range that is missing from the array. 
+//Ex: Input= {9,0,1,2,4,7,3,6,5}
+
+#include<iostream>
+#include<
